adbc7c337ecaf3095343aa5473d31f40647251b5
shiaoal <shiaoal@gmail.com>
2025-12-14 10:30:45 +0800

    remove out folder

--- out/_next/static/chunks/07854f0ea77a2224.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/5b501e20463f3fb1.js",
-  "static/chunks/dd3fd3f6bd21bb7e.js",
-  "static/chunks/b08bc3b8132d7477.js",
-  "static/chunks/turbopack-31a6b025bdc9eb85.js"
-])
--- out/_next/static/chunks/98dc33c6d36ad099.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/5df55a6312d60f42.js",
-  "static/chunks/b08bc3b8132d7477.js",
-  "static/chunks/dd3fd3f6bd21bb7e.js",
-  "static/chunks/eecde347dfbc73ae.js",
-  "static/chunks/turbopack-8442e3b6a4893053.js"
-])
