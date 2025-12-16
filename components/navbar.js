0d2ec3ad6193306f379553635ecc17a09e2f7c45
shiaoal <shiaoal@gmail.com>
2025-12-16 09:24:34 +0800

    1. fix blocker dark layer unter context menu not fill all viewport issue. 2. make head bar 60% opacity

--- components/navbar.js
+++ components/navbar.js
@@ -28,28 +28,14 @@ export default function Navbar() {
   }, []);
 
   return (
-    <div
-      className={`fixed w-full z-50 left-0 top-0 transition-all duration-300 ${scroll ? "bg-white dark:bg-gray-900 shadow-lg" : "bg-transparent"
-        }`}>
-      <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
-        {/* Logo  */}
-        <Disclosure>
-          {({ open }) => (
-            <>
-              <AnimatePresence>
-                {open && (
-                  <Disclosure.Button
-                    as={motion.div}
-                    static
-                    initial={{ x: "100%", opacity: 0 }}
-                    animate={{ x: 0, opacity: 1 }}
-                    exit={{ x: "100%", opacity: 0 }}
-                    transition={{ duration: 0.5, ease: "easeInOut" }}
-                    className="fixed inset-0 z-0 bg-black/80 w-full h-full touch-none cursor-default"
-                    aria-hidden="true"
-                  />
-                )}
-              </AnimatePresence>
+    <Disclosure>
+      {({ open }) => (
+        <>
+          <div
+            className={`fixed w-full z-50 left-0 top-0 transition-all duration-300 ${scroll ? "bg-white/60 dark:bg-gray-900 shadow-lg backdrop-blur-md" : "bg-transparent"
+              }`}>
+            <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
+              {/* Logo  */}
               <div className="flex flex-wrap items-center justify-between w-full lg:w-auto relative z-10">
                 <Link
                   href="/"
@@ -152,40 +138,54 @@ export default function Navbar() {
                   )}
                 </AnimatePresence>
               </div>
-            </>
-          )}
-        </Disclosure>
 
-        {/* menu  */}
-        <div className="hidden text-center lg:flex lg:items-center">
-          <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
-            {navigation.map((menu, index) => (
-              <li className="mr-3 nav__item" key={index}>
+              {/* menu  */}
+              <div className="hidden text-center lg:flex lg:items-center">
+                <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
+                  {navigation.map((menu, index) => (
+                    <li className="mr-3 nav__item" key={index}>
+                      <Link
+                        href="/"
+                        className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none">
+                        {menu}
+                      </Link>
+                    </li>
+                  ))}
+                  <li className="mr-3 nav__item">
+                    <DebugMenu onOpenModal={() => setShowAboutModal(true)} />
+                  </li>
+                </ul>
+              </div>
+
+              <div className="hidden mr-3 space-x-3 lg:flex nav__item">
                 <Link
                   href="/"
-                  className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none">
-                  {menu}
+                  className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
+                  預約參觀
                 </Link>
-              </li>
-            ))}
-            <li className="mr-3 nav__item">
-              <DebugMenu onOpenModal={() => setShowAboutModal(true)} />
-            </li>
-          </ul>
-        </div>
 
-        <div className="hidden mr-3 space-x-3 lg:flex nav__item">
-          <Link
-            href="/"
-            className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
-            預約參觀
-          </Link>
-
-          <ThemeChanger />
-        </div>
-      </nav>
-      <AboutModal isOpen={showAboutModal} onClose={() => setShowAboutModal(false)} />
-    </div>
+                <ThemeChanger />
+              </div>
+            </nav>
+            <AboutModal isOpen={showAboutModal} onClose={() => setShowAboutModal(false)} />
+          </div>
+          <AnimatePresence>
+            {open && (
+              <Disclosure.Button
+                as={motion.div}
+                static
+                initial={{ x: "100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: "100%", opacity: 0 }}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+                className="fixed inset-0 z-40 bg-black/80 w-full h-full touch-none cursor-default"
+                aria-hidden="true"
+              />
+            )}
+          </AnimatePresence>
+        </>
+      )}
+    </Disclosure>
   );
 }
 
