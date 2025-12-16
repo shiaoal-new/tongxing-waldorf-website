4ceb1a41de444175b0f33572f197ae7fc58e4c20
shiaoal <shiaoal@gmail.com>
2025-12-16 10:53:03 +0800

    improve hero background video loading speed

--- components/hero.js
+++ components/hero.js
@@ -11,6 +11,7 @@ export default function Hero() {
         muted
         playsInline
         poster="./img/video-poster.webp"
+        preload="metadata"
         className="absolute top-0 left-0 w-full h-full object-cover z-0 "
       >
         <source
