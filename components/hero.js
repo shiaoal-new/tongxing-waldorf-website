61073b37d21f5ca57d323c44bb65c67f6eb633a4
shiaoal <shiaoal@gmail.com>
2025-12-16 10:02:15 +0800

    perf: 将视频海报图片从 JPG 格式替换为 WEBP 格式，并更新 hero 组件引用。

--- components/hero.js
+++ components/hero.js
@@ -10,7 +10,7 @@ export default function Hero() {
         loop
         muted
         playsInline
-        poster="./img/video-poster.jpg"
+        poster="./img/video-poster.webp"
         className="absolute top-0 left-0 w-full h-full object-cover z-0 "
       >
         <source
